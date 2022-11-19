df87cf0f14e5914be109caaf72b7bc6f12ed19e3
Bennyc0 <benny.chen@csedge.orggi config --global user.name Bennyc0git config --global user.email benny.chen@csedge.org>
2022-11-19 20:47:59 +0000

    working

--- app.py
+++ app.py
@@ -7,27 +7,36 @@ led = LED(17)
 last_cmd = "off"
 
 @app.route('/')
-def index():
+def index_page():
+    global active
+    active = False
     return render_template('index.html')
 
 @app.route('/about')
-def about():
+def about_page():
     return render_template('about.html')
 
 @app.route('/received', methods=['POST'])
-def received():
-    cmd = request.form['light_cmd'].lower()
+def received_page():
+    global active
+    if request.form['light_cmd'].lower() != "":
+        cmd = request.form['light_cmd'].lower()
 
-    if cmd == "on" or cmd == "blink" or cmd == "off":
-        if user != last_cmd:
-            if user == "on":
+        if cmd == "on" or cmd == "blink" or cmd == "off":
+            active = False
+            if cmd == "on":
                 led.on()
-            elif user == "off":
+            elif cmd == "blink":
+                active = True
+                while active:
+                    led.on()
+                    sleep(0.5)
+                    led.off()
+                    sleep(0.5)
+            elif cmd == "off":
                 led.off()
-            last_cmd = cmd
 
-        else:
-            print(f"Hey! The lightbulb is already {last_cmd}! \n")
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port='7000')
+    app.run(debug=True, host='0.0.0.0', port='8000')
